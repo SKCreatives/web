4dc8b1999c2836b76f8f3d7c47f8ab8c1d0250da
tmslnz <t@theworkers.net>
2012-12-20 20:18:55 +0000

    Fluo

--- app.js
+++ app.js
@@ -1,33 +1,112 @@
+/*global */
 
-/**
- * Module dependencies.
- */
+// Session GC on timeout
+// function sessionCleanup() {
+//     sessionStore.all(function(n, s) {
+//         for (var i = 0; i < s.length; i++)
+//             sessionStore.get(s[i], function() {} );
+//     }
+// }
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var http    = require('http');
+var path    = require('path');
+var express = require('express');
+var lessMW  = require('less-middleware');
+var args    = require('commander');
 
-var app = express();
+express.statik = express.static;
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 11690);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+// App
+
+var app = module.exports = express();
+
+
+
+// Mode
+var dev = false;
+if (app.get('env') === 'development') dev = true;
+
+
+
+// Shared
+
+app.locals.siteName = 'Sidekick';
+
+
+
+// Commander
+
+args
+  .version('0.0.1')
+  .option('-p, --port <port>',          'specify the port [11690]',           Number, 11690)
+  .option('-t, --ptitle <Proc_Title>',  'specify the process title [myapp]',  String, 'nordicarch.com(live)')
+  .parse(process.argv);
+
+
+
+
+// Configuration
+
+process.title = args.ptitle;
+
+app.set('port', args.port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
+// Middleware
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(function (req, res, next) {
+  if (req.path.match(/^.*\.less/gi)) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Content-type", "text/less");
+  }
+  next();
+});
+app.use(lessMW({ src: __dirname + '/public' }));
+app.use(express.directory(path.join(__dirname, 'public'), { icons:true }));
+app.use(express.statik(path.join(__dirname, 'public')));
+app.use(function (err, req, res, next) {
+  if (err) res.render('404', {
+      status: 404
+    , err: err
+    , req: req
+  });
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+
+// Set environment
+
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+}
+
+
+
+// Routing
+
+var routes = require('./routes');
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+
+
+// Start server
+http.createServer(app)
+  .listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+  });
+
+
