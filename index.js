2cd82bc94d043a921beee922d99f214dbd40376e
Tommaso Lanza <t@theworkers.net>
2014-01-13 01:10:14 +0000

    Enabled Dropbox as a storage engine

--- index.js
+++ index.js
@@ -1,37 +1,110 @@
-var os           = require('os');
-var fs           = require('fs');
-var path         = require('path');
-var express      = require('express');
-var connect      = require('connect');
-var jade         = require('jade');
-var lessCSS      = require('less-middleware');
-var marked       = require('marked');
-var yaml         = require('js-yaml');
-var async        = require('async');
-var colors       = require('colors');
-var favicons     = require('connect-favicons');
-var nowww        = require('connect-no-www');
-var cheerio      = require('cheerio');
-var _            = require('lodash');
-var config       = require('config');
-var appPackage   = require('./package.json');
-var scraper      = require('./lib/Scraper.js');
+var os       = require('os');
+var fs       = require('fs');
+var path     = require('path');
+var http     = require('http');
+var express  = require('express');
+var connect  = require('connect');
+var jade     = require('jade');
+var lessCSS  = require('less-middleware');
+var marked   = require('marked');
+var yaml     = require('js-yaml');
+var async    = require('async');
+var colors   = require('colors');
+var favicons = require('connect-favicons');
+var nowww    = require('connect-no-www');
+var cheerio  = require('cheerio');
+var _        = require('lodash');
+var config   = require('config');
+var Dropbox  = require('dropbox');
+var scraper  = require('./lib/Scraper.js');
+var Project  = require('./lib/Project.js');
+var Storage  = require('./lib/Storage.js');
 
-var app = module.exports  = express();
+var app = module.exports = express();
+var port = process.env.PORT || 3000;
+var credentials;
+var dropbox;
 
+try {
+  credentials = require('./credentials.json');
+} catch (e) {
+  credentials = {
+    DROPBOX_APP_KEY: process.env.DROPBOX_APP_KEY,
+    DROPBOX_APP_SECRET: process.env.DROPBOX_APP_SECRET,
+    DROPBOX_TOKEN: process.env.DROPBOX_TOKEN
+  };
+}
+
+_.each(credentials, function(val, key) {
+  if (!val) {
+    throw new Error("Missing credential for " + key);
+  }
+});
+
+var storageOptions = {
+  credentials:credentials,
+  appRoot: __dirname
+};
+var storage = new Storage(config.storage, storageOptions);
+var projects = [];
+var documents = {};
+
+// Load campaigns
+storage.readFile('projects.yaml', {encoding:'utf8'}, function(err, data) {
+  yaml.loadAll(data, function(doc) {
+    doc.storageURI = config.storage;
+    doc.storageOptions = storageOptions;
+
+    var project = new Project(doc);
+    projects.push(project);
+  });
+
+  scraper = scraper({ projects: projects });
+  scraper.start();
+});
+
+// Load documents
+storage.readdir('documents', function(err, filenames, dirStat, stats) {
+  console.log(err, stats)
+});
+
+// Set up longpoll_delta for dropbox storage
+if (storage.protocol === 'dropbox') {
+  dropbox = new Dropbox.Client({
+    key: credentials.DROPBOX_APP_KEY,
+    secret: credentials.DROPBOX_APP_SECRET,
+    token: credentials.DROPBOX_TOKEN
+  });
+
+  dropbox.pullChanges(null, function(err, changes) {
+    console.log(err, changes);
+    dropbox.pollForChanges(changes.cursorTag, {}, function(err, result, two) {
+      console.log(err, result, two);
+    });
+  });
+
+}
+
+
+
+
+
+
+// Express config
+_.extend(app.settings, config);
+app.disable('x-powered-by');
+app.locals.settings['views'] = __dirname + '/views';
+app.locals.settings['views engine'] = 'jade';
+app.locals.pretty = false;
+app.locals.basedir = __dirname;
+
+
+// View utils
 app.locals.marked = marked;
 
 
-// Config
-_.extend(app.settings, config);
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-_(app.locals).extend({
-  pretty: (app.get('env') === 'development'),
-  basedir: __dirname
-});
-
+// View data
+app.locals.projects = projects;
 
 
 // Middleware
@@ -50,41 +123,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 
-
-
-
-// Load all projects and site data
-var dataDir = __dirname + '/data';
-var dataFiles = fs.readdirSync(dataDir);
-
-var projects = app.locals.projects = (function() {
-  var buf = fs.readFileSync(dataDir + '/projects.yaml', 'utf8');
-  var docs = [];
-  yaml.loadAll(buf, function (doc) {
-    docs.push(doc);
-  });
-  return docs;
-}());
-
-var site = app.locals.site = [];
-
-scraper = scraper({ projects: projects });
-scraper.start();
-
-
-
-
-
 // Routing
-app.get('/', function(req, res){
+
+app.get('/oauth_callback', function(req, res) {
+  console.log(req.params, req.param);
+});
+
+app.get('/', function(req, res) {
   res.render('index');
 });
 
 
-
 // Start server
-app.listen(app.get('port'));
-console.log("Express server listening on", app.get('port'));
+http.createServer(app).listen(port);
+console.log('Listening on port ' + port);
 
 
 
