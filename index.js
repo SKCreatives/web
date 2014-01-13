8ebab9e061c8bea75a2bd1452437a3d8a66a9e11
Tommaso Lanza <t@theworkers.net>
2014-01-13 15:04:19 +0000

    Implemented longpoll_delta and auto-refresh for docs and campaigns

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var async    = require('async');
 var colors   = require('colors');
 var favicons = require('connect-favicons');
 var nowww    = require('connect-no-www');
+var request  = require('request');
 var cheerio  = require('cheerio');
 var _        = require('lodash');
 var config   = require('config');
@@ -22,6 +23,7 @@ var Storage  = require('./lib/Storage.js');
 
 var app = module.exports = express();
 var port = process.env.PORT || 3000;
+var noop = function() {};
 var credentials;
 var dropbox;
 
@@ -56,6 +58,7 @@ scraper.start();
 // function 
 // Load campaigns
 function loadCampaigns(callback) {
+  callback = callback || noop;
   storage.readFile('projects.yaml', {encoding:'utf8'}, function(err, data) {
     if (err) return callback(err);
 
@@ -90,9 +93,145 @@ loadCampaigns(function(err, campaigns) {
 });
 
 // Load documents
-storage.readdir('documents', function(err, filenames, dirStat, stats) {
-  console.log(err, stats)
+function loadDocuments(callback) {
+  callback = callback || noop;
+  storage.readdir('documents', function(err, filenames, dirStat, stats) {
+    if (err) return callback(err);
+
+    async.each(
+      stats,
+      function iterator(stat, fn) {
+        storage.readFile('documents/' + stat.name, {encoding: 'utf8'}, function(err, data) {
+          if (err) {
+            console.error(err);
+            return fn(null);
+          }
+
+          switch (path.extname(stat.name)) {
+            case '.markdown':
+            case '.md':
+            case '.mdd':
+              documents[stat.name.replace(path.extname(stat.name), '')] = data;
+              fn(null);
+            break;
+            default:
+              fn(null);
+          }
+        });
+      },
+      function done(err) {
+        callback(err, documents);
+      }
+    );
+  });
+}
+
+
+loadDocuments(function(err, docs) {
+  console.log(docs);
 });
+
+
+// Set up longpoll_delta for dropbox storage
+
+// SAMPLE /delta response
+// { has_more: false,
+//   cursor: 'AAGMGdckdwksp26gctmUj51Pydzd3LEUgCQefhdeJ07ql2RTjc2U5NGa8U-iKITnoxmPix8ls8qXEGECarLP3xEFhRdXXCtaexYUq62noiYnXFjo-fBpQEHOJ3KnBPB0A0rzTKIOKS3Ez1XXBVNsAQ0F',
+//   entries: 
+//    [ [ '/apps/sk/cache/90fa9edeb2ece231c818059ae755a4f4.json',
+//        [Object] ] ],
+//   reset: false }
+
+// SAMPLE /longpoll_delta response
+// { hasChanges: true, retryAfter: 0, backoff: 60 }
+
+if (storage.protocol === 'dropbox') {
+
+  dropbox = new Dropbox.Client({
+    key: credentials.DROPBOX_APP_KEY,
+    secret: credentials.DROPBOX_APP_SECRET,
+    token: credentials.DROPBOX_TOKEN
+  });
+
+  var retryAfter = 0; // Seconds
+  var cursor;
+  var entries;
+  var append;
+
+  async.forever(
+    
+    function fn(callback) {
+      setTimeout(function() {
+        request.post({ uri: 'https://api.dropbox.com/1/delta', qs: { cursor: cursor || undefined, access_token: credentials.DROPBOX_TOKEN, path_prefix: '/' + storage.root }}, function(err, res, body) {
+
+          // If we have an error wait one minute and retry
+          if (err) {
+            retryAfter = 60 * 1000;
+            return callback(null);
+          }
+
+          try {
+            body = JSON.parse(body);
+          } catch (e) {
+            retryAfter = 60 * 1000;
+            return callback(null);
+          }
+
+          cursor = body.cursor;
+          append = body.has_more;
+
+          if (append) {
+            entries = (entries && entries.length) ? entries.concat(body.entries) : body.entries;
+            retryAfter = 0;
+            return callback(null);
+          } else {
+            entries = body.entries;
+          }
+
+          // find out where the change has happened
+          for (var i = 0, entry, filename, basename; i < entries.length; i++) {
+            entry = entries[i];
+            filename = entry[0];
+            basename = path.basename(filename);
+
+            if (basename === 'projects.yaml') {
+              loadCampaigns();
+              break;
+            }
+
+            if (filename.match(new RegExp('documents/', 'i'))) {
+              loadDocuments(function(err, docs) {
+                console.log(docs);
+              });
+              break;
+            }
+          }
+
+          // Start a new /longpoll_delta request
+          dropbox.pollForChanges(cursor, function(err, retry, seconds) {
+            console.log('----------------------')
+            console.log(arguments)
+            if (err) {
+              retryAfter = (res) ? (res.retryAfter !== void 0) ? res.retryAfter : (res.backoff !== void 0) ? res.backoff : 60 : 60;
+              return callback(null);
+            }
+
+            if (res.hasChanges) {
+              retryAfter = res.retryAfter;
+              callback(null);
+            } else {
+              retryAfter = 0;
+              callback(null);
+            }
+
+          });
+        });
+      }, retryAfter);
+    },
+    
+    function cb(err) {/* never called */}
+  );
+}
 
 
 
@@ -114,6 +253,7 @@ app.locals.marked = marked;
 
 // View data
 app.locals.projects = projects;
+app.locals.documents = documents;
 
 
 // Middleware
