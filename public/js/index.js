4f18593cd3807b5ac682f10bc4b8c8497fb9a3ab
Tommaso Lanza <t@theworkers.net>
2014-01-20 15:32:01 +0000

    Fixed graphs

--- public/js/index.js
+++ public/js/index.js
@@ -1,49 +1,44 @@
-/*global Raphael, SK */
+/*global Raphael, SK, moment */
 
 $(function($) {
-	
+
+  var SK = window.SK = window.SK || {};
+  var now = moment();
+
   // Raphael day stats
-  var $stats = $('.stats');
-  $stats.each(function(i, el) {
+  $('.stats').each(function(i, el) {
     var $el = $(el);
-    var width = 200;
-    var height = 200;
+    
+    var data = $el.data();
+
+    // console.log(data.pledged, data.goal)
     
     var $backers = $el.find('.backers .graph');
-    var backersGraph = new SK.BackersGraph($backers);
-    backersGraph.update(2);
-
     var $days = $el.find('.days-remaining .graph');
-    var daysremain = parseInt($el.find('.data.days').text(), 10);
-    var duration = parseInt($el.find('.data.duration').text(), 10);
-    var elapsed = duration - daysremain;
-    var daysGraph = new SK.DaysGraph($days[0], 98, duration, 200, 200);
-    daysGraph.update(elapsed === duration ? duration - 0.1 : elapsed);
-
     var $pledges = $el.find('.pledges .graph');
-    var pledged = parseInt($el.find('.data.pledged').text(), 10);
-    var goal = parseInt($el.find('.data.goal').text(), 10);
-    var pledgesGraph = new SK.PledgesGraph($pledges[0], goal);
-    pledgesGraph.update(pledged);
+    var $back = $el.find('.back-project .graph');
+
+    var daysTotal = moment(data.endTime).diff(moment(data.launchTime), 'days');
+    var daysLeft = moment(data.endTime).diff(now, 'days');
+        daysLeft = daysLeft > 0 ? daysLeft : 0;
+    var daysElapsed = daysTotal - daysLeft;
+
+    var backersGraph = new SK.BackersGraph($backers);
+    var daysGraph = new SK.DaysGraph($days[0], 98, daysTotal, 200, 200);
+    var pledgesGraph = new SK.PledgesGraph($pledges[0], data.goal);
+
+    backersGraph.update(data.backersCount);
+    daysGraph.update(daysElapsed);
+    pledgesGraph.update(data.pledged, data.pledgedString);
 
   });
 
-	// Send form
-	$('#subForm').submit(function (e) {
+  // Send form
+  $('#subForm').submit(function (e) {
     e.preventDefault();
-    $.getJSON(
-    this.action + "?callback=?",
-    $(this).serialize(),
-    function (data) {
-      if (data.Status === 400) {
-        alert("Error: " + data.Message);
-      } else { // 200
-        alert("Success: " + data.Message);
-      }
+    $.getJSON(this.action + "?callback=?", $(this).serialize(), function (data) {
+      if (data.Status >= 400) return new Error(data.Status + ' ' + data.Message);
     });
   });
-
-
-
 
 });
--- public/js/lib/Stats.js
+++ public/js/lib/Stats.js
@@ -1,136 +1,139 @@
 /*global Raphael */
 
 (function() {
-  window.SK = window.SK || {};
+  var SK = window.SK = window.SK || {};
 
-  var DaysGraph = window.SK.DaysGraph = function(el, r, duration, w, h) {
-    var self = this;
-    self.r = r || 100;
-    self.duration = duration;
-    self.w = w || r * 2;
-    self.h = h || r * 2;    
-    self.cx = w/2;
-    self.cy = h/2;
-    self.paper = Raphael(el, w, h);
+  SK.DaysGraph = function(el, r, duration, w, h) {
+    this.r = r || 100;
+    this.duration = duration;
+    this.w = w || r * 2;
+    this.h = h || r * 2;    
+    this.cx = w/2;
+    this.cy = h/2;
+    this.paper = Raphael(el, w, h);
 
-    var c = self.paper.circle(self.cx, self.cy, self.r);
+    var c = this.paper.circle(this.cx, this.cy, this.r);
     c.attr({
       'fill': 'url(/img/stats-dots-white.png)',
       'stroke': 'white',
       'stroke-width': 3
     });
 
-    self.sector = self.update(0);
-    self.sector.attr({
+    this.sector = this.update(0);
+    this.sector.attr({
       'fill': 'white',
       'stroke': 'white',
       'stroke-width': 3
     });
   };
 
-  DaysGraph.prototype.update = function(value) {
-    var self = this;
-    var startAngle = 90- (360 * value / self.duration);
+  SK.DaysGraph.prototype.update = function(value) {
+    if (value === void 0) console.error('Invalid value for DaysGraph.update(value)');
+    var elapsedRatio = (value / this.duration === 1) ? 0.999 : value / this.duration;
+    var startAngle = 90 - (360 * elapsedRatio);
     var endAngle = 90;
     var rad = Math.PI / 180;
-    var x1 = self.cx + self.r * Math.cos(-startAngle * rad);
-    var y1 = self.cy + self.r * Math.sin(-startAngle * rad);
-    var x2 = self.cx + self.r * Math.cos(-endAngle * rad);
-    var y2 = self.cy + self.r * Math.sin(-endAngle * rad);
-    var path = ["M", self.cx, self.cy, "L", x1, y1, "A", self.r, self.r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"];
-    
-    if (!self.sector) {
-      return self.paper.path(path);
+    var x1 = this.cx + this.r * Math.cos(-startAngle * rad);
+    var y1 = this.cy + this.r * Math.sin(-startAngle * rad);
+    var x2 = this.cx + this.r * Math.cos(-endAngle * rad);
+    var y2 = this.cy + this.r * Math.sin(-endAngle * rad);
+    var path = ["M", this.cx, this.cy, "L", x1, y1, "A", this.r, this.r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"];
+
+    if (!this.sector) {
+      return this.paper.path(path);
     } else {
-      self.sector.attr('path', path);
+      this.sector.attr('path', path);
     }
   };
 
 
 
-  var BackersGraph = window.SK.BackersGraph = function(el) {
-    var self = this;
-    self.$el = (el instanceof jQuery) ? el : $(el);
-    self.$ghost = $('<div class="text" style="position: absolute; visibility: hidden; height: auto; width: auto;">');
-    self.$text = $('<div class="text">');
-    var pos = self.$el.css('position');
+  SK.BackersGraph = function(el) {
+    this.$el = (el instanceof jQuery) ? el : $(el);
+    this.$ghost = $('<div class="text" style="position: absolute; visibility: hidden; height: auto; width: auto;">');
+    this.$text = $('<div class="text">');
+    var pos = this.$el.css('position');
     if (pos !== 'absolute' || pos !== 'relative') {
-      self.$el.css('position', 'relative');
+      this.$el.css('position', 'relative');
     }
-    self.$el.css({ 'line-height': self.$el.height() + 'px', 'text-align': 'center' });
-    self.$text.css({ 'vertical-align': 'top', 'display':'inline-block' });
-    self.$el.append(self.$ghost);
-    self.$el.append(self.$text);
-    self.update('0');
+    this.$el.css({ 'line-height': this.$el.height() + 'px', 'text-align': 'center' });
+    this.$text.css({ 'vertical-align': 'top', 'display':'inline-block' });
+    this.$el.append(this.$ghost);
+    this.$el.append(this.$text);
+    this.update('0');
   };
 
-  BackersGraph.prototype.update = function(value) {
-    var self = this;
-    self.$ghost.text(value);
-    var totalWidth = self.$el.width();
-    var width = self.$ghost.outerWidth(true);
-    var fontSize = parseInt(self.$text.css('font-size'), 10);
+  SK.BackersGraph.prototype.update = function(value) {
+    this.$ghost.text(value);
+    var totalWidth = this.$el.width();
+    var width = this.$ghost.outerWidth(true);
+    var fontSize = parseInt(this.$text.css('font-size'), 10);
     var maxFontSize = 68;
     if (totalWidth < width) {
       while (totalWidth < width) {
         fontSize -= 1;
-        self.$ghost.css('font-size', fontSize);
-        width = self.$ghost.outerWidth(true);
+        this.$ghost.css('font-size', fontSize);
+        width = this.$ghost.outerWidth(true);
       }
     } else {
       while (totalWidth > width && fontSize < maxFontSize) {
         fontSize += 1;
-        self.$ghost.css('font-size', fontSize);
-        width = self.$ghost.outerWidth(true);
+        this.$ghost.css('font-size', fontSize);
+        width = this.$ghost.outerWidth(true);
       }
     }
-    self.$text.css('font-size', fontSize);
-    self.$text.text(value);
+    this.$text.css('font-size', fontSize);
+    this.$text.text(value);
   };
 
 
 
-  var PledgesGraph = window.SK.PledgesGraph = function(el, total, currency) {
-    var self = this;
-    self.total = total || 0;
-    self.currency = currency || '£';
-    self.$el = (el instanceof jQuery) ? el : $(el);
-    var pos = self.$el.css('position');
+  SK.PledgesGraph = function(el, total) {
+    if (total === void 0) console.error('Invalid total for PledgesGraph(el, total)');
+    this.$el = (el instanceof jQuery) ? el : $(el);
+    this.total = total;
+
+    var pos = this.$el.css('position');
     if (pos !== 'absolute' || pos !== 'relative') {
-      self.$el.css('position', 'relative');
+      this.$el.css('position', 'relative');
     }
-    self.$gauge = $('<div class="gauge" style="position: absolute; bottom: 0; width: 100%">');
-    self.$text = $('<div class="text" style="width:100%; text-align: center;">');
-    self.$text.css({ 'vertical-align': 'top', 'display':'inline-block' });
-    self.$gauge.append(self.$text);
-    self.$el.append(self.$gauge);
+    
+    this.$gauge = $('<div class="gauge" style="position: absolute; bottom: 0; width: 100%">');
+    this.$text = $('<div class="text" style="width:100%; text-align: center;">');
+    
+    this.$text.css({ 'vertical-align': 'top', 'display':'inline-block' });
+    this.$gauge.append(this.$text);
+    this.$el.append(this.$gauge);
   };
 
-  PledgesGraph.prototype.update = function(value) {
-    var self = this;
-    var totalHeight = self.$el.height();
-    var gaugeHeight = totalHeight * value / self.total;
+  SK.PledgesGraph.prototype.update = function(value, valueString) {
+    if (value === void 0) console.error('Undefined value for PledgesGraph.update(value, valueString)');
+    if (valueString === void 0) console.error('Undefined valueString for PledgesGraph.update(value, valueString)');
+
+    var totalHeight = this.$el.height();
+    var gaugeHeight = totalHeight * value / this.total;
+
     if (gaugeHeight > totalHeight) { gaugeHeight = totalHeight }
 
-    self.$text.text(self.currency + value + ' pledged');
-    var textHeight = self.$text.height();
+    this.$text.text(valueString + ' pledged');
+    var textHeight = this.$text.height();
     if (gaugeHeight < textHeight) {
-      self.$text.css({
+      this.$text.css({
         'position': 'absolute',
         'top': -textHeight -20,
         'color': 'white',
         'text-shadow': '0px 0px 2px rgb(0,30,97), 0px 0px 10px rgb(0,77,251)'
       });
     } else {
-      self.$text.css({
+      this.$text.css({
         'position': 'relative',
         'top': 'auto',
         'color': 'rgb(0,77,251)',
         'text-shadow': 'none'
       });
     }
-    self.$gauge.height(gaugeHeight);
-    self.$gauge.css({ 'line-height': gaugeHeight + 'px' });
+    this.$gauge.height(gaugeHeight);
+    this.$gauge.css({ 'line-height': gaugeHeight + 'px' });
   };
 
 
